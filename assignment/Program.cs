80c7f60518e05c2ab16df24719eef657122b1892
omar <om145028@gmail.com>
2024-12-30 17:25:26 -0800

    Add project files.

---
+++ assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            Console.Write("enter num");
+            int num1;
+            int.TryParse(Console.ReadLine(),out  num1);
+            if (num1%3==0 && num1%4==0)
+            {
+                Console.Write("yes");
+            }
+            else
+            {
+                Console.Write("no");
+            }
+            #endregion
+
+
+
+
+        }
+    }
+}
